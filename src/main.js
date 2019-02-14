f490089b59a33df00c486ebd16ca4984d473d157
Alex <ajhollid@gmail.com>
2019-02-14 09:36:01 -0800

    Refacoterd view code into ViewUtils module

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
-console.log( 'test' );
+require( '../styles/application.scss' );
+const ViewUtils = require( './view_utils' );
+
+const app = ( function () {
+  ViewUtils.setupCanvases( window.innerWidth, window.innerHeight );
+}() );
 
